426cea605b5a532ddcbfc3d34c98c50888d360a5
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 16:49:27 +0300

    problem 1 marked

--- cpp/ps0001/cpp_ps0001_solution.cpp
+++ cpp/ps0001/cpp_ps0001_solution.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
+	// prompting user for 3 number to add 0/2
 	int x=155, y=940, e=168, z;
+
+	// outputting the result of the summation problems 3/3
 	z = x + y + e;
 	cout << "the solution is :\n" << z;
+	
 	return 0;
-
 }
+// good work 👍 7/10
